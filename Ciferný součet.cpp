4ba21b3e999fb15bcfd6762a242c5d45b73b9f2d
stepa <stepa@NB-STEPAN>
2022-02-27 20:56:22 +0100

    Přidejte soubory projektu.

---
+++ Ciferný součet.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+// Ciferný součet pomocí smyčky while
+int cif_soucet(int x) {
+	int y, vysledek = 0;
+	while (x > 0) {
+		y = x % 10;
+		vysledek = vysledek + y;
+		x = x / 10;
+	}
+	return vysledek;
+}
+// Vypsání výsledku
+int main() {
+	printf("cif soucet je %d", cif_soucet(573));
+}
